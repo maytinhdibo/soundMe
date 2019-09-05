de8d49c0d223f19cea3a8e7a5f061f798a5be543
maytinhdibo <iammaytinhdibo@gmail.com>
2019-09-05 23:36:32 +0700

    swipe navigation

--- App.js
+++ App.js
@@ -1,36 +1,84 @@
-import React from 'react';
-import { StyleSheet, Text, View,ScrollView, Image } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Platform,Text, View, ScrollView, Image } from 'react-native';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, SafeAreaView } from 'react-navigation';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import { changeNavigationBarColor } from 'react-native-navigation-bar-color';
+
 import Home from './page/Home';
 import Search from './page/Search';
+import { registerRootComponent } from 'expo';
 
+try {
+  if (Platform.OS == 'android') {
+    changeNavigationBarColor('#ffffff');
+  }
+} catch (e) {
+  console.log(e)// {success: false}
+}
+
+console.ignoredYellowBox = true;
 function Profile() {
   return (
     <View style={styles.container}>
-       <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
       <Text>Profile!</Text>
     </View>
   );
 }
 
-const AppNavigator = createMaterialBottomTabNavigator(
+const AppNavigator = createMaterialTopTabNavigator(
   {
     Feed: Home,
     Search: Search,
     Help: Home,
   },
   {
+    tabBarPosition: 'bottom',
+    swipeEnable: true,
     initialRouteName: 'Feed',
     activeColor: '#f0edf6',
     inactiveColor: '#444321',
-    barStyle: { backgroundColor: '#fff' },
+    tabBarOptions: {
+      indicatorStyle: {
+        backgroundColor: '#f72',
+        top: 0,
+      },
+      labelStyle: {
+        fontSize: 12,
+        color: "#345",
+      },
+      tabStyle: {
+        // width: 100,
+      },
+      style: {
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        backgroundColor: '#fff',
+      },
+    }
   }
 );
 
-export default AppContainer = createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView forceInset={{ top: 'never' }} style={{ flex: 1 }}>
+        <AppContainer />
+      </SafeAreaView>
+    )
+  }
+}
 
 
 const styles = StyleSheet.create({
