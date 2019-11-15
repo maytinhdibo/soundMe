7586e923b3a6e50e896c8ed5c12a3afab6ba3200
maytinhdibo <iammaytinhdibo@gmail.com>
2019-11-15 10:49:50 +0700

    library full page

--- pages/Library.js
+++ pages/Library.js
@@ -12,16 +12,179 @@ import { createAppContainer } from "react-navigation";
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import { textStyle } from "../styles/textStyle";
 import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import CardView from "react-native-cardview";
 
-const FirstRoute = () => (
-  <View style={[{ backgroundColor: "#ff4081" }]}>
-    <Text>ahihi</Text>
+class PlayListLibItem extends Component {
+  render() {
+    return (
+      <View
+        style={{ flexDirection: "row", alignItems: "center", marginBottom: 9 }}
+      >
+        <CardView
+          cardElevation={2}
+          cornerRadius={9}
+          style={{
+            backgroundColor: "#eee",
+            height: 60,
+            width: 60,
+            marginRight: 14,
+          }}
+        >
+          <Image
+            source={this.props.imgUrl || require("../assets/music.png")}
+            style={{
+              height: "100%",
+              width: "100%",
+              resizeMode: "cover",
+              width: "100%",
+            }}
+          />
+        </CardView>
+        <View style={{ flexDirection: "column" }}>
+          <Text style={[{ fontSize: 15 }, textStyle.bold]}>
+            {this.props.name}
+          </Text>
+          <Text style={[{ fontSize: 14, color: "#777" }, textStyle.regular]}>
+            {this.props.count} bài hát
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+class AlbumLibItem extends Component {
+  render() {
+    return (
+      <View
+        style={{ flexDirection: "row", alignItems: "center", marginBottom: 9 }}
+      >
+        <CardView
+          cardElevation={2}
+          cornerRadius={9}
+          style={{
+            backgroundColor: "#eee",
+            height: 60,
+            width: 60,
+            marginRight: 14,
+          }}
+        >
+          <Image
+            source={this.props.imgUrl || require("../assets/music.png")}
+            style={{
+              height: "100%",
+              width: "100%",
+              resizeMode: "cover",
+              width: "100%",
+            }}
+          />
+        </CardView>
+        <View style={{ flexDirection: "column" }}>
+          <Text style={[{ fontSize: 15 }, textStyle.bold]}>
+            {this.props.name}
+          </Text>
+          <Text style={[{ fontSize: 14, color: "#777" }, textStyle.regular]}>
+            {this.props.artist}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+class ArtistLibItem extends Component {
+  render() {
+    return (
+      <View
+        style={{ flexDirection: "row", alignItems: "center", marginBottom: 9 }}
+      >
+        <CardView
+          cardElevation={2}
+          cornerRadius={30}
+          style={{
+            backgroundColor: "#eee",
+            height: 60,
+            width: 60,
+            marginRight: 14,
+          }}
+        >
+          <Image
+            source={this.props.imgUrl || require("../assets/thuphuong.jpg")}
+            style={{
+              height: "100%",
+              width: "100%",
+              resizeMode: "cover",
+              width: "100%",
+            }}
+          />
+        </CardView>
+        <View style={{ flexDirection: "column" }}>
+          <Text style={[{ fontSize: 15 }, textStyle.bold]}>
+            {this.props.name}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const SongRoute = () => (
+  <View style={{ padding: 7, paddingHorizontal: 16 }}>
+    <View
+      style={{ flexDirection: "row", alignItems: "center", marginBottom: 9 }}
+    >
+      <CardView
+        cardElevation={2}
+        cornerRadius={9}
+        style={{
+          backgroundColor: "#eee",
+          height: 60,
+          width: 60,
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: 14,
+        }}
+      >
+        <Text style={[{ fontSize: 32, color: "#aaa" }]}>+</Text>
+      </CardView>
+      <Text style={[{ fontSize: 15 }, textStyle.bold]}>
+        Tạo danh sách phát mới
+      </Text>
+    </View>
+    <PlayListLibItem count={10} name={"Nhạc buồn"} />
+    <PlayListLibItem count={5} name={"Vinahouse quẩy tung chảo"} />
+    <PlayListLibItem count={2} name={"Oppa Idol"} />
   </View>
 );
 
-const SecondRoute = () => (
-  <View style={[{ backgroundColor: "#fff081" }]}>
-    <Text>ahoho</Text>
+const ArtistRoute = () => (
+  <View style={{ padding: 7, paddingHorizontal: 16 }}>
+    <ArtistLibItem name={"Thu Phương"} />
+    <ArtistLibItem name={"Bích Phương"} />
+    <ArtistLibItem name={"Trúc Nhân"} />
+  </View>
+);
+
+const AlbumRoute = () => (
+  <View style={{ padding: 7, paddingHorizontal: 16 }}>
+    <AlbumLibItem
+      imgUrl={require("../assets/nuocmat.jpg")}
+      artist="Hoàng Thùy Linh"
+      name={"Hoàng"}
+    />
+    <AlbumLibItem
+      imgUrl={{
+        uri:
+          "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/2/1/4/b/214b84c68b94865dbc8e908f75449c79.jpg",
+      }}
+      artist="Nhiều ca sĩ"
+      name={"Tiếng Hát Thiên Thai"}
+    />
+    <AlbumLibItem
+      imgUrl={require("../assets/nuocmat.jpg")}
+      artist="Tâm 9"
+      name={"Mỹ Tâm"}
+    />
   </View>
 );
 
@@ -44,11 +207,13 @@ class Tabs extends Component {
             >
               <View
                 style={{
-                  padding: 6,
+                  padding: 9,
                   paddingHorizontal: 12,
                   marginEnd: 6,
                   backgroundColor:
-                    navigation.state.index === index ? "#f21" : "transparent",
+                    navigation.state.index === index
+                      ? "#fe6f61"
+                      : "transparent",
                   color: "#fff",
                   borderRadius: 20,
                 }}
@@ -60,7 +225,7 @@ class Tabs extends Component {
                   style={[
                     {
                       fontSize: 14,
-                      color: navigation.state.index === index ? "#fff" : "#000",
+                      color: navigation.state.index === index ? "#fff" : "#777",
                     },
                     textStyle.bold,
                   ]}
@@ -79,13 +244,13 @@ class Tabs extends Component {
 const TabNavigator = createMaterialTopTabNavigator(
   {
     "Bài hát yêu thích": {
-      screen: FirstRoute,
+      screen: SongRoute,
     },
     "Ca sĩ": {
-      screen: SecondRoute,
+      screen: ArtistRoute,
     },
-    "Album": {
-      screen: FirstRoute,
+    Album: {
+      screen: AlbumRoute,
     },
   },
   {
