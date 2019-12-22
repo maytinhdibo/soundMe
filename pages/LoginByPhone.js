8f627a2eeb468bcfbfdcd7e78abb5d41130daba7
Huong99 <43237489+Huong99@users.noreply.github.com>
2019-12-23 00:04:13 +0700

    add Login

---
+++ pages/LoginByPhone.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import MeIcon from "../icons/MeIcon";
+import mePlay from "../icons/icon-pack/mePlay";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Image
+} from 'react-native';
+
+export default class LoginByPhone extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <MeIcon style={[styles.icon, styles.inputIcon]} icon={mePlay} size={25} color="blue" />
+          <TextInput style={styles.inputs}
+              placeholder="Name"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <MeIcon style={[styles.icon, styles.inputIcon]} icon={mePlay} size={25} color="blue" />
+          <TextInput style={styles.inputs}
+              placeholder="Phone number"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'/>
+        </View>
+     
+        <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}
+          onPress={() => this.props.navigation.navigate("OTPInput")}
+        >
+          <Text style={styles.loginText}>Tiếp tục</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#B0E0E6',
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:350,
+      height:45,
+      marginBottom:15,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  icon:{
+    width:30,
+    height:30,
+  },
+  inputIcon:{
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:350,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: '#3498db',
+  },
+  loginText: {
+    color: 'white',
+  },
+});
