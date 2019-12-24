298df39e8394c87f28b0bcbd3c438a519ef78773
maytinhdibo <iammaytinhdibo@gmail.com>
2019-12-24 23:52:08 +0700

    fix some bug

--- assets/data/playlistItem.js
+++ assets/data/playlistItem.js
@@ -1,8 +1,8 @@
 export default {
     data: [
-        {name: "Top song Hương Tràm", image: require("../PlaylistItem/baihathuongtram.jpg")},
+        {name: "Hương Tràm hát", image: require("../PlaylistItem/baihathuongtram.jpg")},
         {name: "Ngốc (Single)", image: require("../PlaylistItem/ngochuongtram.jpg")},
-        {name: "Người từng yêu anh rất sâu nặng", image: require("../PlaylistItem/nguoitungyeuratsaudam.jpg")},
+        {name: "Duyên Mình Lỡ", image: require("../PlaylistItem/nguoitungyeuratsaudam.jpg")},
         {name: "Với Em Là Mãi Mãi", image: require("../PlaylistItem/voiemlamaimai.jpg")},
     ]
 }
