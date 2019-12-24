4775f826b21ac72a0d9b7d0cd8cbbcca44701ddf
Anh  DH <anhdhbn@gmail.com>
2019-12-24 15:07:50 +0700

    fix issue

---
+++ assets/data/playlistItem.js
@@ -0,0 +1,8 @@
+export default {
+    data: [
+        {name: "Top song Hương Tràm", image: require("../PlaylistItem/baihathuongtram.jpg")},
+        {name: "Ngốc (Single)", image: require("../PlaylistItem/ngochuongtram.jpg")},
+        {name: "Người từng yêu anh rất sâu nặng", image: require("../PlaylistItem/nguoitungyeuratsaudam.jpg")},
+        {name: "Với Em Là Mãi Mãi", image: require("../PlaylistItem/voiemlamaimai.jpg")},
+    ]
+}
